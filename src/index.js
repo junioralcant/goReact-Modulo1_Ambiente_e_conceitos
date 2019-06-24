1087c05bc9343e1375f4e4c045c1a614aa9d2d92
Junior Marques <junioralcant111@gmail.com>
2019-06-24 15:48:42 -0300

    6 Propriedades children

--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@ import { render } from 'react-dom';
 
 class Button extends Component {
     render() {
-        return <a href="">Salvar</a>;
+        return <a href="" onClick={this.props.onClick}>{this.props.children}</a>;
     }
 }
 
 class App extends Component {
+
+    handlerClick(){
+        alert('Botão Clicado')
+    }
+
     render() {
         return (
             <div>
                 <h1>Hello World</h1>
-                <Button />
+                <Button onClick={this.handlerClick}>Enviar</Button>
             </div>
         );
     }
